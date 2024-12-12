5b533c56be29cd26bc894a543eb4377507e1b17d
gdy-12 <gungdeyudi880@gmail.com>
2024-12-12 12:08:36 +0800

    Pertemuan 6

---
+++ pertemuan604 Lingkaran.py
@@ -0,0 +1,18 @@
+print ("Menghitung Luas dan Keliling Lingkaran")
+
+#Input nilai jari-jari
+r = float (input("Masukan Nilai Jari-Jari (r): "))
+
+#Nilai Phi
+phi = 3.14
+
+#Rumus
+luas = phi * r * r
+kll = 2 * phi * r
+
+#Proses hitung nilai Luas dan keliling
+print ("Luas = 3.14 x r x r")
+print ("Luas = ",luas)
+print ("Keliling = 2 x 3.14 x r")
+print ("Keliling = ",kll)
+
---
+++ pertemuan605 Pitagoras.py
@@ -0,0 +1,12 @@
+print ("Mencari Nilai C dengan Pitagoras")
+
+import math
+a = float (input ("Input Nilai A = "))
+b = float (input ("Input Nilai B = "))
+#Rumus
+c = a **2 + b **2
+
+
+#Input Nilai A dan B
+print ("Nilai c^2 = a^2 + b^2")
+print ("Nilai c = ", math.sqrt(c))
