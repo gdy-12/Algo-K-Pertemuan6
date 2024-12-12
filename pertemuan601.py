4eb3d35783826f141ca6c76c38318afb7cc28435
gdy-12 <gungdeyudi880@gmail.com>
2024-12-12 12:06:52 +0800

    Upload Pertemuan 6

---
+++ pertemuan601.py
@@ -0,0 +1,51 @@
+a = 3
+b = 10
+
+#Penjumlahan
+print (a + b)
+
+#Pengurangan
+print (b - a)
+
+#Perkalian
+print (a * b)
+
+#Pembagian
+print (a/b)
+
+
+import math
+
+#Menghitung akar kuadrat dari x
+print (math.sqrt(25))
+
+#Menghitung x^y
+print (math.pow(4,5))
+
+#Menghitung Nilai Absolut (mutlak)
+print (math.fabs(-8))
+
+#Menghitung Faktorial
+print (math.factorial(10))
+
+#Menghitung Nilai Pembulatan Keatas
+print (math.ceil(5.8))
+
+#Menghitung Nilai Pemulatan Kebawah
+print (math.floor(4.3))
+
+#Menghitung sinus dari x (dalam radian).
+print(math.sin(math.radians(30)))
+
+#Menghitung kosinus dari x (dalam radian).
+print(math.cos(math.radians(30)))
+
+#
+Menghitung tangen dari x (dalam radian).
+print(math.tan(math.radians(45)))  
+
+#Mengonversi derajat ke radian.
+print(math.radians(45))  
+
+#Mengonversi radian ke derajat.
+print(math.degrees(math.pi / 4))  
---
+++ pertemuan602.py
@@ -0,0 +1,19 @@
+import math
+
+#Menghitung akar kuadrat dari x
+print (math.sqrt(25))
+
+#Menghitung x^y
+print (math.pow(4,5))
+
+#Menghitung Nilai Absolut (mutlak)
+print (math.fabs(-8))
+
+#Menghitung Faktorial
+print (math.factorial(10))
+
+#Menghitung Nilai Pembulatan Keatas
+print (math.ceil(5.8))
+
+#Menghitung Nilai Pemulatan Kebawah
+print (math.floor(4.3))
